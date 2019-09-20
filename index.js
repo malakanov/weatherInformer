a4871bdb0ca48278ad9e6c52ec93633d2075c450
Sergey Malakanov <sergey.malakanov@gmail.com>
2019-09-20 09:29:02 +0500

    port add to listen

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.post("/", async (req, res) => {
 
 let port = process.env.PORT || 3000
 //Запуск сервера на 3000 порту
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server started on port", port);
 });
  
